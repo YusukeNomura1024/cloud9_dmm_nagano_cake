0c8ae2cfa3ff495439f9be901b05ffc16064e8fb
EC2 Default User <ec2-user@ip-172-31-42-23.ec2.internal>
2021-08-10 11:49:20 +0000

    [Add] enum and has_many

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
+  
+  attachment :image
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  enum making_status: { 製作不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3 }
 end
