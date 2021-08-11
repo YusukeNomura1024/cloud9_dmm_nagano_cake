86b1a5d195876c17179e2b4ee5f552b5cacb106c
EC2 Default User <ec2-user@ip-172-31-42-23.ec2.internal>
2021-08-11 03:40:16 +0000

    close#3

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
+  #管理者用のため新規登録とパスワードリセット機能は削除
 end
